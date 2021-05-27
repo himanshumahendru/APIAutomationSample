9abefbf96823c8f14c1bbe652cf19ff02192da22
himanshumahendru <him.mahendru@gmail.com>
2021-05-27 23:53:18 +0530

    third commit

--- src/test/java/APIEndpoints/CreateCustomer.java
+++ src/test/java/APIEndpoints/CreateCustomer.java
@@ -9,7 +9,7 @@ import java.util.Hashtable;
 public class CreateCustomer extends BaseTest {
 	
 	public static Response sendPostRequestToCreateCustomerAPIWithValidAuthKey(Hashtable<String,String> data) {
-		
+		// test comment 
 		Response response = given().auth().basic("sk_test_51HxdAsB2gNNCSqnW0f0xuix3aOTZUqjqH7q2DN3JA4lEXmA35K0IoLz4e0Kjnmwn94R7OCJwNd2SCHyZoclmVu1s00hob0KYGc","").log().all()
 				.formParam("email",data.get("email")).formParam("description", data.get("description"))
 				.post(config.getProperty("customerAPIEndpoint"));
